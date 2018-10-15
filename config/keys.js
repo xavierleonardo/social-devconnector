b638022791bce6fe9db15f758079636d6f75f908
xavierleonardo <gxavier.leonardo@gmail.com>
2018-10-15 00:18:51 -0300

    Web token generate

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,5 @@
 module.exports = {
-  mongoURI: "mongodb://admin:123qaz@ds227243.mlab.com:27243/developersconnector"
+  mongoURI:
+    "mongodb://admin:123qaz@ds227243.mlab.com:27243/developersconnector",
+  secretOrKey: "secret"
 };
