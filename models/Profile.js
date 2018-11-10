600aeee211f8d263e4e3d6837eb2df2e3c4c2914
xavierleonardo <gxavier.leonardo@gmail.com>
2018-11-10 16:40:38 -0200

    Add experience and education to profile

--- models/Profile.js
+++ models/Profile.js
@@ -53,7 +53,7 @@ const ProfileSchema = new Schema({
         required: true
       },
       to: {
-        type: String
+        type: Date
       },
       current: {
         type: Boolean,
@@ -79,7 +79,7 @@ const ProfileSchema = new Schema({
         required: true
       },
       to: {
-        type: String
+        type: Date
       },
       current: {
         type: Boolean,
