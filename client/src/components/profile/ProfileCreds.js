2d2f7c0b5c61999ef85ad7b1e174c43f5771a3be
xavierleonardo <gxavier.leonardo@gmail.com>
2018-11-20 19:42:54 -0200

    GitHub latest repos fetching and component creating

--- client/src/components/profile/ProfileCreds.js
+++ client/src/components/profile/ProfileCreds.js
@@ -9,7 +9,8 @@ class ProfileCreds extends Component {
       <li key={exp._id} className="list-group-item">
         <h4>{exp.company}</h4>
         <p>
-          <Moment format="YYYY/MM/DD">{exp.from}</Moment> -
+          <Moment format="YYYY/MM/DD">{exp.from}</Moment>
+          {" - "}
           {exp.to === null ? (
             "now"
           ) : (
@@ -42,7 +43,8 @@ class ProfileCreds extends Component {
       <li key={edu._id} className="list-group-item">
         <h4>{edu.school}</h4>
         <p>
-          <Moment format="YYYY/MM/DD">{edu.from}</Moment> -
+          <Moment format="YYYY/MM/DD">{edu.from}</Moment>
+          {" - "}
           {edu.to === null ? (
             "now"
           ) : (
--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -11,7 +11,7 @@ class Profiles extends Component {
   }
 
   render() {
-    const { profiles, loading } = this.props.profile;
+    const { profiles } = this.props.profile;
     let profileItems;
 
     if (profiles === null) {
