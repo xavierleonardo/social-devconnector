4df30f69d5f6f78c07912eb2031bf821f71bcd76
xavierleonardo <gxavier.leonardo@gmail.com>
2018-11-20 18:42:44 -0200

    Profile about and creds display

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -35,7 +35,10 @@ class Profile extends Component {
           </div>
           <ProfileHeader profile={profile} />
           <ProfileAbout profile={profile} />
-          <ProfileCreds profile={profile} />
+          <ProfileCreds
+            education={profile.education}
+            experience={profile.experience}
+          />
           <ProfileGithub profile={profile} />
         </div>
       );
