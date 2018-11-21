7b84cd94ef4fa3e7986b6c3f43b25c78a771af3c
xavierleonardo <gxavier.leonardo@gmail.com>
2018-11-21 19:33:20 -0200

    Security config

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: 'mongodb://admin:123qaz@ds227243.mlab.com:27243/developersconnector',
-  secretOrKey: 'secret'
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
